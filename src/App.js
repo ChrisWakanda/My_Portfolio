c4b2948d97c8bd4c2d6c61cbe6058ce9bd1e99bf
Aryan Raveshia <aryan.raveshia12@gmail.com>
2025-04-12 21:28:02 +0530

    Initial commit: React portfolio setup with CSS

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Hello, I am Aryan Raveshia.
+      </h1>
+      <p>
+      This is my Portfolio Website. Welcome!
+      </p>
+    </div>
+  );
+}
+
+export default App;
