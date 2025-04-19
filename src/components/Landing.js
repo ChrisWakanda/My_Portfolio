85e1bedb5d69ae6dce10ca5c0975a2805bb8361e
Aryan Raveshia <aryan.raveshia12@gmail.com>
2025-04-19 16:13:14 +0530

    landing page V3

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,8 +3,9 @@ import React from 'react';
 //import landing_pg_img from '../assets/undraw_coding_joxb.svg';
 //import landing_pg_img from '../assets/undraw_in-the-zone_07y7.svg';
 
-
-
+import linkedin_icon from '../assets/icons8-linkedin-48.png';
+import github_icon from '../assets/icons8-github-48.png';
+import x_icon from '../assets/icons8-x-48.png';
 
 
 function Landing() {
@@ -13,8 +14,21 @@ function Landing() {
 
       <div className="side-toolbar">
         <h1 className="logo">
-              AIR
+            AIR
         </h1>
+
+        <button className='linkedin_button' onClick={()=>window.open('https://www.linkedin.com/in/aryanraveshia/', '_blank')}>
+          <img src={linkedin_icon} alt='linkedin_icon' className='linkedin_icon'></img>
+        </button>
+
+        <button className='github_button' onClick={()=>window.open('https://github.com/ChrisWakanda', '_blank')}>
+          <img src={github_icon} alt='github_icon' className='github_icon'></img>
+        </button>
+
+        <button className='x_button' onClick={()=>window.open('https://x.com/aryan_raveshia', '_blank')}>
+          <img src={x_icon} alt='x_icon' className='x_icon'></img>
+        </button>
+
       </div>
 
       <div className="main-container">
@@ -36,6 +50,8 @@ function Landing() {
           <span className="builder-text">builder</span> at heart. <br />
           <span className="global-text">global</span> in spirit.
         </h2>
+
+
       </div>
     </>
   );
